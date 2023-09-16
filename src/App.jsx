45fec4cf8627d875f6d7c7ec0b0feae68e5285a2
njugunamwangi <njugunamwangi.n@gmail.com>
2023-09-16 14:52:20 +0300

    start timer

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,20 @@ function padTime(time) {
     return time.toString().padStart(2, '0')
 }
 
-function App() {
-    const start = () => {
+export default function App() {
+    const [ title, setTitle ] = useState('Let the countdown begin')
+    const [ timeLeft, setTimeLeft ] = useState(10)
 
+    function startTimer() {
+        setInterval(() => {
+            setTimeLeft(
+                timeLeft => {
+                    if (timeLeft >= 1) return timeLeft - 1
+
+                    return 0
+                }
+            )
+        }, 1000)
     }
 
     const stop = () => {
@@ -18,30 +29,26 @@ function App() {
 
     }
 
-    const [ timeLeft, setTimeLeft ] = useState(25*60)
     const minutes = padTime(Math.floor(timeLeft / 60))
     const seconds = padTime((timeLeft - minutes*60))
 
     return (
-        <>
-            <h1>
-                <span>{minutes}</span>
-                <span>:</span>
-                <span>{seconds}</span>
-            </h1>
-            <div className="card">
-                <button onClick={start}>
-                    Start
-                </button>
-                <button onClick={stop}>
-                    Stop
-                </button>
-                <button onClick={reset}>
-                    Reset
-                </button>
+        <div className="flex h-screen items-center justify-evenly">
+            <div className="flex flex-col items-center">
+                    <h2 className="text-2xl font-semibold">{title}</h2>
+                    <h1 className="text-4xl m-4 font-semibold" id="timer">
+                        <span>{minutes}</span>
+                        <span>:</span>
+                        <span>{seconds}</span>
+                    </h1>
+
+                <div className="flex space-x-4">
+                    <button onClick={startTimer} className="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-lg shadow-md">Start</button>
+                    <button className="bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded-lg shadow-md">Stop</button>
+                    <button className="bg-gray-500 hover:bg-gray-600 text-white py-2 px-4 rounded-lg shadow-md">Reset</button>
+                </div>
             </div>
-        </>
+        </div>
+
     )
 }
-
-export default App
